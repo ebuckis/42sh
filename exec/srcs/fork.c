0c6adbf9c64a2c2af766abeb056e0594e8850d08
Baptiste Pajot <bpajot@z4r5p3.le-101.fr>
2018-10-17 11:30:02 +0200

    gestion pipe propre

--- exec/srcs/fork.c
+++ exec/srcs/fork.c
@@ -6,12 +6,40 @@
 /*   By: kcabus <kcabus@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/05/29 10:59:08 by bpajot       #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/12 14:04:43 by bpajot      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/17 11:27:04 by bpajot      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "exec.h"
+
+static int		**ft_make_tab_pipe_fd(int nb_pipe)
+{
+	int		**tab_pipe_fd;
+	int		i;
+	int		pipeline[2];
+
+	tab_pipe_fd = (int**)malloc(sizeof(int*) * (nb_pipe + 2));
+	tab_pipe_fd[nb_pipe + 1] = NULL;
+	i = -1;
+	while (++i <= nb_pipe)
+	{
+		tab_pipe_fd[i] = (int*)malloc(sizeof(int) * 2);
+		if (i > 0)
+		{
+			pipe(pipeline);
+			tab_pipe_fd[i - 1][1] = pipeline[1];
+			tab_pipe_fd[i][0] = pipeline[0];
+		}
+	}
+	tab_pipe_fd[0][0] = 0;
+	tab_pipe_fd[nb_pipe][1] = 1;
+	i = -1;
+	//while (++i <= nb_pipe)
+	//	ft_printf("tab_pipe_fd[%d][0] = %d [%d][1] = %d\n", i, tab_pipe_fd[i][0],
+	//		i, tab_pipe_fd[i][1]);
+	return(tab_pipe_fd);
+}
 
 /*
 ** ft_fork_pipe : cree le pipeline et fork
@@ -21,30 +49,44 @@
 ** on fait appel a cette fonction autant de fois qu'il y a de pipe
 */
 
-static int		ft_fork_pipe(t_parse *p, int *tab_pipe, char ***p_env, int i)
+static int		ft_fork_pipe(t_parse *p, int tab_pipe_i, char ***p_env,
+		int **tab_pipe_fd, int i)
 {
 	int		pid;
-	int		pipeline[2];
+	int		j;
 
-	pipe(pipeline);
 	pid = fork();
-	if (pid < 0)
+	if (pid == 0)
 	{
-		close(pipeline[0]);
-		close(pipeline[1]);
-		ft_putendl_fd("error", 2);
-	}
-	else if (pid == 0)
-	{
-		dup2(pipeline[1], STDOUT_FILENO);
-		close(pipeline[0]);
-		ft_execve(p, tab_pipe[i], p_env);
+		if (tab_pipe_fd[i][0] != 0)
+		{
+		//	ft_printf("dup2(pipe[0] = %d, STDIN)\n", tab_pipe_fd[i][0]);
+			dup2(tab_pipe_fd[i][0], STDIN_FILENO);
+		}
+		if (tab_pipe_fd[i][1] != 1)
+		{
+		//	ft_printf("dup2(pipe[1] = %d, STDOUT)\n", tab_pipe_fd[i][1]);
+			dup2(tab_pipe_fd[i][1], STDOUT_FILENO);
+		}
+		j = -1;
+		while(tab_pipe_fd[++j])
+		{
+			if (j != i && tab_pipe_fd[j][0] != 0)
+				close(tab_pipe_fd[j][0]);
+			if (j != i && tab_pipe_fd[j][1] != 1)
+				close(tab_pipe_fd[j][1]);
+		}
+		//ft_printf("execve: %s\n", p->arg[tab_pipe_i]);
+		ft_execve(p, tab_pipe_i, p_env);
 	}
 	else if (pid > 0)
 	{
-		dup2(pipeline[0], STDIN_FILENO);
-		close(pipeline[1]);
-		return (pid);
+		if (tab_pipe_fd[i][0] != 0)
+			close(tab_pipe_fd[i][0]);
+		if (tab_pipe_fd[i][1] != 1)
+			close(tab_pipe_fd[i][1]);
+		//ft_printf(" %s -> pid = %d\n", p->arg[tab_pipe_i], pid);
+		return(pid);
 	}
 	return (0);
 }
@@ -58,24 +100,29 @@ static int		ft_fork_pipe(t_parse *p, int *tab_pipe, char ***p_env, int i)
 */
 
 static void		ft_fork_shell3(t_parse *p, int *tab_pipe, int *pid_tab,
-		int pid)
+		int** tab_pipe_fd)
 {
 	int			i;
 	int			status;
 
-	waitpid(pid, &status, WUNTRACED);
 	i = -1;
 	while (pid_tab[++i])
-		;
-	while (--i >= 0)
 	{
-		if ((ft_strequ(p->arg[tab_pipe[i]], "yes")) ||
-				(ft_strequ(p->arg[tab_pipe[i]], "top")) ||
-				(ft_strequ(p->arg[tab_pipe[i]], "base64")))
-			kill(pid_tab[i], 9);
-		waitpid(pid_tab[i], NULL, WUNTRACED);
+		//ft_printf("pid = %d waiting\n", pid_tab[i]);
+		waitpid(pid_tab[i], &status, WUNTRACED);
+		//ft_printf("pid = %d finish\n", pid_tab[i]);
+		if (tab_pipe_fd[i][0] != 0)
+		{
+		//	ft_printf("close tab_pipe_fd[%d][0] = %d\n", i, tab_pipe_fd[i][0]);
+			close(tab_pipe_fd[i][0]);
+		}
+		if (tab_pipe_fd[i][1] != 1)
+		{
+		//	ft_printf("close tab_pipe_fd[%d][1] = %d\n", i, tab_pipe_fd[i][1]);
+			close(tab_pipe_fd[i][1]);
+		}
 	}
-	exit(ft_ret_display(p, pid, status, p->arg[tab_pipe[0]]));
+	exit(ft_ret_display(p, pid_tab[i], status, p->arg[tab_pipe[0]]));
 }
 
 /*
@@ -84,33 +131,40 @@ static void		ft_fork_shell3(t_parse *p, int *tab_pipe, int *pid_tab,
 */
 
 static void		ft_fork_shell2(t_parse *p, int *tab_pipe, char ***p_env,
-		int nb_pip)
+		int nb_pipe)
 {
-	int			i;
 	int			*pid_tab;
-	pid_t		pid;
+	int			**tab_pipe_fd;
+	int			i;
+	//int			pipeline[2];
 
-	i = -1;
-	if ((pid_tab = (int*)malloc(sizeof(int) * (nb_pip + 1))))
+	pid_tab = (int*)malloc(sizeof(int) * (nb_pipe + 2));
+	pid_tab[nb_pipe + 1] = 0;
+	tab_pipe_fd = ft_make_tab_pipe_fd(nb_pipe);
+	i = nb_pipe + 1;
+	while ( --i >= 0)
 	{
-		pid_tab[nb_pip] = 0;
-		i = nb_pip + 1;
-		while (--i > 0)
-			pid_tab[nb_pip - i] = ft_fork_pipe(p, tab_pipe, p_env, nb_pip - i);
-		if (nb_pip)
-		{
-			pid = fork();
-			if (pid == 0)
-				ft_execve(p, tab_pipe[nb_pip], p_env);
-			else if (pid > 0)
-				ft_fork_shell3(p, tab_pipe, pid_tab, pid);
-		}
-		else
-			ft_execve(p, tab_pipe[nb_pip], p_env);
-		ft_memdel((void**)&pid_tab);
+		//pipeline[0] = tab_pipe_fd[i][0];
+		//pipeline[1] = tab_pipe_fd[i][1];
+		pid_tab[i] = ft_fork_pipe(p, tab_pipe[i], p_env, tab_pipe_fd, i);
 	}
-	else
-		exit(1);
+		//ft_printf("i = %d pid = %d commande = %s\n", i, pid_tab[i], p->arg[tab_pipe[i]]);
+		//	}
+		//	pid = fork();
+		//	if (pid == 0)
+		//	{
+		//		ft_printf("execve: %s\n", p->arg[tab_pipe[nb_pip]]);
+		//		ft_execve(p, tab_pipe[nb_pip], p_env);
+		//	}
+		//	else if (pid > 0)
+		ft_fork_shell3(p, tab_pipe, pid_tab, tab_pipe_fd);
+		//}
+		//else
+		//	ft_execve(p, tab_pipe[nb_pip], p_env);
+	ft_memdel((void**)&pid_tab);
+	//}
+	//else
+	//	exit(1);
 }
 
 /*
