d39db77ee4eea56a21c0e820eb2f993c77c44ba7
Baptiste Pajot <bpajot@z4r5p3.le-101.fr>
2018-11-06 10:28:11 +0100

    norme cd

--- exec/srcs/cd.c
+++ exec/srcs/cd.c
@@ -6,7 +6,7 @@
 /*   By: kcabus <kcabus@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/06/04 10:50:52 by bpajot       #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/27 15:43:52 by bpajot      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/06 10:24:23 by bpajot      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,18 +17,18 @@
 **  Mise a jour du PWD et OLDPWD in env
 */
 
-static int      actualise_env(char *new_pwd, char ***p_env)
+static int		actualise_env(char *new_pwd, char ***p_env)
 {
-    char    *pwd;
+	char	*pwd;
 
-    pwd = NULL;
-    pwd = ft_getpwd(*p_env, 0);
-    if (new_pwd)
-	    ft_setpwd(*p_env, 0, new_pwd);
-    if (pwd)
-	    ft_setpwd(*p_env, 1, pwd);
-    ft_strdel(&pwd);
-    return (0);
+	pwd = NULL;
+	pwd = ft_getpwd(*p_env, 0);
+	if (new_pwd)
+		ft_setpwd(*p_env, 0, new_pwd);
+	if (pwd)
+		ft_setpwd(*p_env, 1, pwd);
+	ft_strdel(&pwd);
+	return (0);
 }
 
 /*
@@ -46,47 +46,47 @@ void			ft_putendl_fd_arg(char *str, char *path)
 ** built-in cd
 */
 
-static int      check_target(char **target, char ***p_env)
+static int		check_target(char **target, char ***p_env)
 {
-    if (*target)
-    {
-        if (ft_strcmp(*target, "cd") == 0)
-        {
-            ft_strdel(target);
-            *target = ft_home(*p_env);
-            if (*target == NULL)
-                ft_putendl_fd("cd: HOME not set", 2);
-        }
-        else if (ft_strcmp(*target, "oldpwd") == 0)
-        {
-            ft_strdel(target);
-            *target = ft_getpwd(*p_env, 1);
-            if (*target == NULL)
-                ft_putendl_fd("cd: OLDPWD not set", 2);
-        }
-    }
-    return (0);
+	if (*target)
+	{
+		if (ft_strcmp(*target, "cd") == 0)
+		{
+			ft_strdel(target);
+			*target = ft_home(*p_env);
+			if (*target == NULL)
+				ft_putendl_fd("cd: HOME not set", 2);
+		}
+		else if (ft_strcmp(*target, "oldpwd") == 0)
+		{
+			ft_strdel(target);
+			*target = ft_getpwd(*p_env, 1);
+			if (*target == NULL)
+				ft_putendl_fd("cd: OLDPWD not set", 2);
+		}
+	}
+	return (0);
 }
 
 int				ft_cd(char **arg, char ***p_env)
 {
-    char        *target;
+	char	*target;
 
-    target = NULL;
-    if (arg[1] == NULL)
-        target = ft_home(*p_env);
-    else
-        target = ret_target(arg, 0, 0, 0);
-    check_target(&target, p_env);
-    if (target == NULL)
-        return (1);
-    if (chdir(target) < 0)
-    {
-        ft_putstr_fd("cd : an error is occured\n", 2);
-        ft_strdel(&target);
-        return (1);
-    }
-    actualise_env(target, p_env);
-    ft_strdel(&target);
+	target = NULL;
+	if (arg[1] == NULL)
+		target = ft_home(*p_env);
+	else
+		target = ret_target(arg, 0, 0, 0);
+	check_target(&target, p_env);
+	if (target == NULL)
+		return (1);
+	if (chdir(target) < 0)
+	{
+		ft_putstr_fd("cd : an error is occured\n", 2);
+		ft_strdel(&target);
+		return (1);
+	}
+	actualise_env(target, p_env);
+	ft_strdel(&target);
 	return (0);
 }
