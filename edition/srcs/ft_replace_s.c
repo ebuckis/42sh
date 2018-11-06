377bc55f87f6e730a9fc56aa303261b5ff72f42a
Kevin Cabus <kcabus@z4r5p5.le-101.fr>
2018-11-06 13:04:40 +0100

    maj de ft_replace_s

--- edition/srcs/ft_replace_s.c
+++ edition/srcs/ft_replace_s.c
@@ -6,7 +6,7 @@
 /*   By: kcabus <kcabus@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/25 16:01:31 by kcabus       #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/26 14:24:22 by kcabus      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/06 13:03:48 by kcabus      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,15 +15,21 @@
 
 int		ft_replace_s(char *s, t_navig *n, int pos)
 {
-	int		end;
-	int		tmp;
+	char	**string;
+	char	*tmp;
+	int		nb;
 
-	end = pos + 1;
-	tmp = 0;
-	while (n->s[pos + end] == s[end])
-		end++;
-	n->s = ft_realloc()
-	return (1);
+	string = (char **)malloc(sizeof(char *) * 4);
+	string[0] = ft_strsub(n->s, 0, (size_t)pos);
+	string[1] = s;
+	string[2] = ft_strdup(n->s + pos + strlen(s));
+	string[3] = NULL;
+	tmp = ft_strjoin(string[0], (char *)string[1]);	
+	nb = ft_strlen(tmp) - pos;
+	ft_strdel(&(n->s));
+	n->s = ft_strjoin(tmp, string[2]);	
+	ft_strdel(&tmp);
+	return (nb);
 }
 
 /*
