2dcf3ab22a66c158a0007487cd482b1d33756149
Kevin Cabus <kcabus@z4r5p6.le-101.fr>
2018-11-08 11:10:50 +0100

    resolution d'un mini segfault

--- edition/srcs/ft_replace_s.c
+++ edition/srcs/ft_replace_s.c
@@ -6,7 +6,7 @@
 /*   By: kcabus <kcabus@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/25 16:01:31 by kcabus       #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/06 13:03:48 by kcabus      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/08 10:48:22 by kcabus      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -24,10 +24,14 @@ int		ft_replace_s(char *s, t_navig *n, int pos)
 	string[1] = s;
 	string[2] = ft_strdup(n->s + pos + strlen(s));
 	string[3] = NULL;
+	dprintf(2, "///|%s|***\n", string[0]);
+	dprintf(2, "///|%s|***\n", string[1]);
+	dprintf(2, "///|%s|***\n", string[2]);
 	tmp = ft_strjoin(string[0], (char *)string[1]);	
 	nb = ft_strlen(tmp) - pos;
 	ft_strdel(&(n->s));
 	n->s = ft_strjoin(tmp, string[2]);	
+	ft_free_tab(&string);
 	ft_strdel(&tmp);
 	return (nb);
 }
