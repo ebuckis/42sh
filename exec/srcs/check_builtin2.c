7177555b0bbfc5ea1fcac55c4d82807c5acd3ede
Baptiste Pajot <bpajot@student.42.fr>
2018-12-03 10:16:49 +0100

    history reforke, touch pas pti con

--- exec/srcs/check_builtin2.c
+++ exec/srcs/check_builtin2.c
@@ -6,7 +6,7 @@
 /*   By: kcabus <kcabus@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/06/01 18:20:01 by bpajot       #+#   ##    ##    #+#       */
-/*   Updated: 2018/12/03 09:51:30 by bpajot      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/12/03 10:16:14 by bpajot      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -24,7 +24,8 @@ int		check_builtin2(char **tab_com)
 	if (tab_com && (ft_strequ(tab_com[0], "cd") || ft_strequ(tab_com[0], "exit")
 		|| ft_strequ(tab_com[0], "setenv") || ft_strequ(tab_com[0], "set")
 		|| ft_strequ(tab_com[0], "unsetenv") || ft_strequ(tab_com[0], "unset")
-		|| ft_strequ(tab_com[0], "export") || ft_strchr(tab_com[0], '=')))
+		|| ft_strequ(tab_com[0], "export") || ft_strchr(tab_com[0], '=')
+		|| ft_strequ(tab_com[0], "history")))
 		return (1);
 	else
 		return (0);
